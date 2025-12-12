5a86c996c463ae948154bf03afe53761d437aae1
giniz123 <giniz.osti@gmail.com>
2025-12-12 22:27:58 +1100

    equals to function (=) added

---
+++ script.js
@@ -0,0 +1,41 @@
+const buttons = document.querySelectorAll(".btn");
+const displayElm = document.querySelector(".display");
+// console.log(buttons);
+let displayString = "";
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const btnText = btn.innerText;
+    buttonAddingToDisplayScreen(btnText);
+  });
+});
+
+const buttonAddingToDisplayScreen = (button) => {
+  if (button === "AC") {
+    console.log(button);
+    displayString = "";
+    return displayfunc(displayString);
+  }
+
+  if (button === "C") {
+    displayString = displayString.slice(0, -1);
+    return displayfunc(displayString);
+  }
+
+  if (button === "=") {
+    return calculation();
+  }
+
+  displayString += button;
+  displayfunc(displayString);
+};
+
+const displayfunc = (string) => {
+  displayElm.innerText = string || "0.0";
+};
+
+const calculation = () => {
+  const total = eval(displayString);
+  displayString = total;
+  return displayfunc(displayString);
+};
